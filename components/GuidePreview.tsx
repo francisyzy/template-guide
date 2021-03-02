f4cdd349410c137965b18123e35a9aa1da449408
ninest <parth.kabra@gmail.com>
2021-03-02 18:58:33 +0800

    Add images

--- components/GuidePreview.tsx
+++ components/GuidePreview.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 const GuidePreview = ({page}) => {
   return (
-    <Link href={`/g/${page.slug}`}>
+    <Link href={`/${page.slug}`}>
     <a className="block p-base border-4 rounded-lg">
       <h3>{page.title}</h3>
       <div className="mt-xs leading-6">{page.description}</div>
